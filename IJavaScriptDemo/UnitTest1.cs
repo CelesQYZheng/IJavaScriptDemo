195690d03631fd32efc0aaca603f4f0fc4d5e345
CelesQYZheng <celes.qy.zheng@gmail.com>
2020-05-06 07:10:33 +0800

    add new methods and comments

--- IJavaScriptDemo/UnitTest1.cs
+++ IJavaScriptDemo/UnitTest1.cs
@@ -43,7 +43,7 @@ namespace IJavaScriptDemo
             // check checkbox
             ((IJavaScriptExecutor)driver).ExecuteScript("document.querySelectorAll('input[value=read]')[0].click()");
             Thread.Sleep(2000);
-            // uncheck the box 
+            // uncheck the checkbox 
             ((IJavaScriptExecutor)driver).ExecuteScript("document.querySelector('input[value=read]').click()");
             Thread.Sleep(2000);
 
@@ -57,7 +57,7 @@ namespace IJavaScriptDemo
             IWebDriver driver = new FirefoxDriver();
             driver.Url = "https://techexpozed.co.nz";
             Thread.Sleep(2000);
-
+            // get the inner text
             var innertext = ((IJavaScriptExecutor)driver).ExecuteScript("return document.documentElement.innerText;").ToString();
             Console.WriteLine(innertext);
             Thread.Sleep(2000);
@@ -69,10 +69,40 @@ namespace IJavaScriptDemo
             IWebDriver driver = new FirefoxDriver();
             driver.Url = "https://techexpozed.co.nz";
             Thread.Sleep(2000);
-
+            // get the title
             var title = ((IJavaScriptExecutor)driver).ExecuteScript("return document.title;").ToString();
 
             Console.WriteLine(title);
+            Thread.Sleep(2000);
+            driver.Quit();
+        }
+
+        [TestMethod]
+        public void JavaScriptGetTheDomain()
+        {
+            IWebDriver driver = new FirefoxDriver();
+            // nevigate to the page
+            driver.Url = "https://techexpozed.co.nz";
+            Thread.Sleep(2000);
+            // get the domain
+            var domain = ((IJavaScriptExecutor)driver).ExecuteScript("return document.domain;").ToString();
+            // print out domain
+            Console.WriteLine(domain);
+            Thread.Sleep(2000);
+            driver.Quit();
+        }
+
+        [TestMethod]
+        public void JavaScriptGetTheURL()
+        {
+            IWebDriver driver = new FirefoxDriver();
+            // nevigate to the page
+            driver.Url = "https://techexpozed.co.nz";
+            Thread.Sleep(2000);
+            // get the URL
+            var url = ((IJavaScriptExecutor)driver).ExecuteScript("return document.URL;").ToString();
+            // print out domain
+            Console.WriteLine(url);
             Thread.Sleep(2000);
             driver.Quit();
         }
